8abc63739152617de02c96d20eb6d203f654e48b
Muqtada Qahtan <muqtada.sniper@gmail.com>
2026-05-13 08:53:03 -0400

    Add files via upload

---
+++ Substitution.c
@@ -0,0 +1,63 @@
+#include "cs50.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+
+    string key = argv[1];
+    if (strlen(key) != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (!isalpha(key[i]))
+        {
+            printf("Key must only contain alphabetic characters.\n");
+            return 1;
+        }
+
+        for (int j = i + 1; j < 26; j++)
+        {
+            if (tolower(key[i]) == tolower(key[j]))
+            {
+                printf("Key must not contain repeated characters.\n");
+                return 1;
+            }
+        }
+    }
+
+    string plaintext = get_string("plaintext:  ");
+    printf("ciphertext: ");
+
+    // 4. عملية التبديل
+    for (int i = 0, n = strlen(plaintext); i < n; i++)
+    {
+        if (isupper(plaintext[i]))
+        {
+            int index = plaintext[i] - 'A';
+            printf("%c", toupper(key[index]));
+        }
+        else if (islower(plaintext[i]))
+        {
+            int index = plaintext[i] - 'a';
+            printf("%c", tolower(key[index]));
+        }
+        else
+        {
+            printf("%c", plaintext[i]);
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
